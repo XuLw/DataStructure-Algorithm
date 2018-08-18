475f4a9f28f437dfda856492195d943b442f8fb6
xulw <2874887061@qq.com>
2018-08-18 09:37:19 +0800

    added level traverse and remove node function

--- src/algorithm/AlgoTest.java
+++ src/algorithm/AlgoTest.java
@@ -2,6 +2,8 @@ package algorithm;
 
 import java.util.Arrays;
 
+import dataStructure.BST;
+
 public class AlgoTest {
 
 	public static final int N = 1000000;
@@ -59,7 +61,41 @@ public class AlgoTest {
 	}
 
 	public static void main(String[] args) {
-		new AlgoTest();
+		// new AlgoTest();
+		int n = 100;
+		BST<Integer, Integer> bst = new BST<>();
+		for (int i = 0; i < n; i++) {
+			Integer key = (int) (Math.random() * n);
+			bst.insert(key, key);
+		}
+
+		// bst.levelOrder();
+
+		System.out.println();
+		Integer order[] = new Integer[n];
+		for (int i = 0; i < n; i++) {
+			order[i] = i;
+		}
+		// 打乱数组
+		for (int i = n - 1; i > 0; i--) {
+			int pos = (int) (Math.random() * (i + 1));
+			int temp = order[pos];
+			order[pos] = order[i];
+			order[i] = temp;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (bst.contain(order[i])) {
+				bst.remove(order[i]);
+				System.out.println("After remove " + order[i] + " size = " + bst.size());
+			}
+		}
+
+		// 最终整个二分搜索树应该为空
+		System.out.println(bst.size());
+
+		// bst.levelOrder();
+
 	}
 
 }
--- src/dataStructure/BST.java
+++ src/dataStructure/BST.java
@@ -1,5 +1,8 @@
 package dataStructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
  * 二分查找树
  */
@@ -22,15 +25,190 @@ public class BST<K extends Comparable, V> {
 	}
 
 	public void insert(K key, V value) {
-		insert(root, key, value);
+		root = insert(root, key, value);
+	}
+
+	// 先序遍历
+	public void preOrder() {
+		preOrder(root);
+	}
+
+	// 中序遍历
+	public void inOrder() {
+		inOrder(root);
+	}
+
+	// 后序遍历非递归实现
+	public void postOrder() {
+		// if (root == null)
+		// return;
+		// Stack<Node> nodeStack = new Stack();
+		// Node temp = root;
+		// do {
+		// if (temp.left != null) {
+		// nodeStack.push(temp);
+		// temp = temp.left;
+		// continue;
+		// } else {
+		// System.out.print(temp.toString() + " ");
+		// temp = nodeStack.pop();
+		// }
+		//
+		// if (temp.right != null) {
+		// nodeStack.push(temp);
+		// temp = temp.right;
+		// continue;
+		// }
+		//
+		// } while (!nodeStack.isEmpty());
+		// TODO
+
+		postOrder(root);
+	}
+
+	/*
+	 * 层序遍历
+	 */
+	public void levelOrder() {
+		Queue<Node> q = new LinkedList<>();
+
+		if (root != null)
+			q.add(root);
+
+		while (!q.isEmpty()) {
+			Node temp = q.poll();
+			if (temp.left != null)
+				q.add(temp.left);
+			if (temp.right != null)
+				q.add(temp.right);
+			System.out.print(temp.toString() + " ");
+		}
+	}
+
+	/*
+	 * 返回最大节点值
+	 */
+	public K maxmum() {
+
+		if (count == 0)
+			return null;
+
+		Node temp = root;
+
+		while (temp != null) {
+
+			if (temp.right == null)
+				break;
+			temp = temp.right;
+
+		}
+		return temp.key;
+	}
+
+	/*
+	 * 返回最小节点值
+	 * 
+	 * 递归写法
+	 */
+	public K minimum() {
+
+		if (count == 0)
+			return null;
+
+		return minimum(root).key;
+
+	}
+
+	/*
+	 * 删除最大值
+	 */
+	public void removeMax() {
+
+		if (root != null)
+			root = removeMax(root);
+
+	}
+
+	/*
+	 * 删除最小值
+	 */
+	public void removeMin() {
+
+		if (root != null)
+			root = removeMin(root);
+
+	}
+
+	/*
+	 * Hubbard Deletion
+	 */
+	public void remove(K key) {
+		root = remove(root, key);
 	}
 
 	public boolean contain(K key) {
 		return contain(this.root, key);
 	}
 
+	/*
+	 * 非递归实现 contain函数
+	 */
+	public boolean containV1(K key) {
+		Node temp = this.root;
+		while (temp != null) {
+			if (key.compareTo(temp.key) == 0)
+				return true;
+			else if (key.compareTo(temp.key) > 0)
+				temp = temp.right;
+			else
+				temp = temp.left;
+		}
+		return false;
+	}
+
 	public V search(K key) {
 		return search(this.root, key);
+	}
+
+	/*
+	 * 非递归实现 search
+	 */
+	public V searchV1(K key) {
+		Node temp = this.root;
+		while (temp != null) {
+			if (key.compareTo(temp.key) == 0)
+				return temp.value;
+			else if (key.compareTo(temp.key) > 0)
+				temp = temp.right;
+			else
+				temp = temp.left;
+		}
+
+		return null;
+	}
+
+	private void preOrder(Node node) {
+		if (node != null) {
+			System.out.print(node.toString() + " ");
+			preOrder(node.left);
+			preOrder(node.right);
+		}
+	}
+
+	private void inOrder(Node node) {
+		if (node != null) {
+			inOrder(node.left);
+			System.out.print(node.toString() + " ");
+			inOrder(node.right);
+		}
+	}
+
+	private void postOrder(Node node) {
+		if (node != null) {
+			postOrder(node.left);
+			postOrder(node.right);
+			System.out.print(node.toString() + " ");
+		}
 	}
 
 	private Node insert(Node node, K key, V value) {
@@ -74,6 +252,85 @@ public class BST<K extends Comparable, V> {
 			return search(node.left, key);
 	}
 
+	private Node minimum(Node node) {
+		if (node.left == null)
+			return node;
+
+		return minimum(node.left);
+	}
+
+	private Node removeMax(Node node) {
+
+		if (node.right == null) {
+			Node leftNode = node.left;
+			node.left = null;
+			count--;
+			return leftNode;
+		}
+
+		node.right = removeMax(node.right);
+		return node;
+	}
+
+	private Node removeMin(Node node) {
+
+		if (node.left == null) {
+			Node rightNode = node.right;
+			node.right = null;
+			count--;
+			return rightNode;
+		}
+
+		node.left = removeMin(node.left);
+		return node;
+	}
+
+	public Node remove(Node node, K key) {
+
+		if (node == null)
+			return null;
+
+		if (key.compareTo(node.key) < 0) {
+			node.left = remove(node.left, key);
+			return node;
+		} else if (key.compareTo(node.key) > 0) {
+			node.right = remove(node.right, key);
+			return node;
+		} else { // key == node->key
+
+			// 待删除节点左子树为空的情况
+			if (node.left == null) {
+				Node rightNode = node.right;
+				node.right = null;
+				count--;
+				return rightNode;
+			}
+
+			// 待删除节点右子树为空的情况
+			if (node.right == null) {
+				Node leftNode = node.left;
+				node.left = null;
+				count--;
+				return leftNode;
+			}
+
+			// 待删除节点左右子树均不为空的情况
+
+			// 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+			// 用这个节点顶替待删除节点的位置
+			Node successor = minimum(node.right);
+			count++;
+
+			successor.right = removeMin(node.right);
+			successor.left = node.left;
+
+			node.left = node.right = null;
+			count--;
+
+			return successor;
+		}
+	}
+
 	private class Node {
 		private K key;
 		private V value;
@@ -84,9 +341,18 @@ public class BST<K extends Comparable, V> {
 			this.value = value;
 			this.left = this.right = null;
 		}
+
+		public Node(Node node) {
+			this.key = node.key;
+			this.value = node.value;
+			this.left = node.left;
+			this.right = node.right;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + key.toString() + " , " + value.toString() + "]";
+		}
 	}
 
-	public void test() {
-		// TODO
-	}
 }
